1e0b5c49128107295bf30243d91a26e927ed3df7
Eric Laitman <ericlaitman@Erics-MacBook-Pro-2.local>
2019-01-06 20:39:38 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,27 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component{
+  constructor(){
+    super()
+      this.state = {
+        timesClicked: 0
+      }
+  }
+
+  onClick = () => {
+    this.setState({
+      timesClicked: this.state.timesClicked + 1
+    });
+  }
+
+  render(){
+    return (
+      <button onClick={this.onClick}>
+        {this.state.timesClicked}
+      </button>
+    )
+  }
+
+
+}//class end
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,68 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component{
+  constructor(){
+    super()
+      this.state = {
+        errors: [],
+        user: null,
+        settings: {
+          bitrate: 8,
+          video: {
+            resolution: '1080p'
+          }
+        }
+      }
+  }
+
+  onClick = () => {
+    this.setState({
+      settings: Object.assign({}, this.state.settings, {
+        bitrate:12
+      })
+    })
+  }
+
+  onClickResolution = () => {
+    this.setState({
+      settings: Object.assign({}, this.state.settings, {
+        video: Object.assign({}, this.state.settings.video, {
+          resolution: '720p'
+        })
+      })
+    })
+  }
+
+//or
+      // this.setState({
+      //   settings: {
+      //     ...this.state.settings,
+      //     video: {
+      //       ...this.state.settings.video,
+      //       resolution: '720p'
+      //     }
+      //   },
+      // });
+
+  render(){
+    return (
+      <div>
+      <button
+        className='bitrate'
+        onClick={this.onClick}
+        >
+        Change Bitrate
+      </button>
+      <button
+        className='resolution'
+        onClick={this.onClickResolution}
+        >
+        Change Resolution
+      </button>
+      </div>
+    )
+  }
+
+
+}//end of class
